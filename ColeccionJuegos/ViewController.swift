b968350140eb71e08dbd0f929a2ca82d2c41b662
Nikoll Bonilla <nikoll.bonilla@tecsup.edu.pe>
2022-05-19 21:50:47 -0500

    Initial Commit

---
+++ ColeccionJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColeccionJuegos
+//
+//  Created by Mac 14 on 19/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
